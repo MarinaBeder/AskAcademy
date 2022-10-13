e400a10bdd73f05a4b3cc27d5961e101bf88e5b7
MarinaBeder <66501215+MarinaBeder@users.noreply.github.com>
2022-10-14 00:12:50 +0200

    Add frist http function

---
+++ src/main/java/com/askacademy/AskAcademy/WelcomeContrller.java
@@ -0,0 +1,13 @@
+package com.askacademy.AskAcademy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeContrller {
+@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to spring boot heroku demo";
+	}
+	
+}
