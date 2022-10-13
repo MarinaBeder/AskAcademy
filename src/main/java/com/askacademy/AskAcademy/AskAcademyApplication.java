e5168f728168fecee8604a16aee367ab732f630b
MarinaBeder <66501215+MarinaBeder@users.noreply.github.com>
2022-10-14 00:02:02 +0200

    Add files via upload

---
+++ src/main/java/com/askacademy/AskAcademy/AskAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.askacademy.AskAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskAcademyApplication.class, args);
+	}
+
+}
